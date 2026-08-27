261f55148f887986deb97d3315960bbae2e1d9b7
joincheol <66054870+joincheol@users.noreply.github.com>
2026-08-28 01:29:05 +0900

    [Passed] 정수 선언하고 뺄셈 (90ms, 9MB)

---
+++ trail1/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 97, b = 13;
+        System.out.println(a+" - "+b+" = "+(a-b));
+    }
+}
