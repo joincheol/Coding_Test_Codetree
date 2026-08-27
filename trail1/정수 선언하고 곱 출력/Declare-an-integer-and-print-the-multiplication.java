1018f18031c74d654a9a0a206e88681ce1d6ff71
joincheol <66054870+joincheol@users.noreply.github.com>
2026-08-28 01:33:12 +0900

    [Passed] 정수 선언하고 곱 출력 (89ms, 9MB)

---
+++ trail1/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int num1 = 26, num2 = 5;
+        System.out.println(num1+" * "+num2+" = "+(num1*num2));
+    }
+}
