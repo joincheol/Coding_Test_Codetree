2f7e2dac05f24369b6a7d7ab4617c77a64ec75a4
joincheol <66054870+joincheol@users.noreply.github.com>
2026-08-28 16:17:45 +0900

    [Passed] 최대공약수 구하기 (131ms, 10MB)

--- trail2/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ trail2/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void cal(int n1, int n2){
         int divide = 1;
-        for(int i=1; i<=n1; i++){
+        for(int i=1; i<=Math.min(n1, n2); i++){
             if(n1 % i == 0 & n2 % i == 0){
                 divide = i;
             }
