622648cecd76a7ed4127b68a6dbcadb505a574c4
joincheol <66054870+joincheol@users.noreply.github.com>
2026-08-28 16:22:05 +0900

    [Passed] 최소공배수 구하기 (140ms, 10MB)

--- trail2/최소공배수 구하기/find-the-least-common-multiple.java
+++ trail2/최소공배수 구하기/find-the-least-common-multiple.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static int cal(int num1, int num2){
         int result = 0;
-        for(int i=1; i<=num2; i++){
-            if((num1 * i) % num2 == 0){
-                result = num1 * i;
-                break;
+        for(int i=1; i<=Math.min(num1, num2); i++){
+            if(num1 % i == 0 & num2 % i == 0){
+                result = i;
             }
         }
-        return result;
+        return num1 * num2 / result;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
