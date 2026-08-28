cd001385b3fd90a8d83880cd974ec46576ad7059
joincheol <66054870+joincheol@users.noreply.github.com>
2026-08-29 00:09:34 +0900

    [Passed] 길이 단위 변환하기 (96ms, 9MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double ft = 9.2;
+        double mile = 1.3;
+
+        System.out.printf("%.1fft = %.1fcm\n", ft, (ft*30.48));
+        System.out.printf("%.1fmi = %.1fcm", mile, (mile*160934));
+    }
+}
