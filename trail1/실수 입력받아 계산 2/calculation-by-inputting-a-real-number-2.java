28b08b28d54f75ac6d76c4ca7d603ea816f6dc8b
joincheol <66054870+joincheol@users.noreply.github.com>
2026-08-29 15:42:11 +0900

    [Passed] 실수 입력받아 계산 2 (147ms, 12MB)

--- trail1/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
+++ trail1/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        Double a = sc.nextDouble();
+        double a = sc.nextDouble();
         System.out.printf("%.2f\n", a+1.5);
     }
 }
