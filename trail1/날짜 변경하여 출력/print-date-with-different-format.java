3ea0c640a80201b0dc872bed37b8b1a3568e313e
joincheol <66054870+joincheol@users.noreply.github.com>
2026-08-30 20:18:47 +0900

    [Passed] 날짜 변경하여 출력 (117ms, 10MB)

---
+++ trail1/날짜 변경하여 출력/print-date-with-different-format.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String[] parts = str.split("[.]");
+
+        System.out.println(parts[1]+"-"+parts[2]+"-"+parts[0]);
+    }
+}
