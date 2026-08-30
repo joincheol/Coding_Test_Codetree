b8e4f17f00d4acffe80d9fe86e9ee68f1ebd7146
joincheol <66054870+joincheol@users.noreply.github.com>
2026-08-30 16:37:23 +0900

    [Passed] 입력받은 값 교체하기 (130ms, 10MB)

--- trail1/입력받은 값 교체하기/changing-inputs.java
+++ trail1/입력받은 값 교체하기/changing-inputs.java
@@ -6,9 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int temp = a;
-        a = b;
-        b = temp;
-        System.out.println(a+" "+b);
+
+        System.out.println(b+" "+a);
     }
 }
