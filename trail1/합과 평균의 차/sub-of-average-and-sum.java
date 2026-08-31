85ccfa3fe3cdb496d48983733b75d805f830e9e5
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-01 00:00:16 +0900

    [Passed] 합과 평균의 차 (140ms, 10MB)

---
+++ trail1/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int total = a+b+c;
+        int avg = total / 3;
+
+        System.out.println(total);
+        System.out.println(avg);
+        System.out.println(total - avg);
+    }
+}
