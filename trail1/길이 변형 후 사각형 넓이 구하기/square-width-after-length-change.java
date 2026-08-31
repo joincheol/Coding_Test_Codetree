d1df7831e3112499f0c8244f54c48b7449553250
joincheol <66054870+joincheol@users.noreply.github.com>
2026-08-31 23:42:37 +0900

    [Passed] 길이 변형 후 사각형 넓이 구하기 (135ms, 10MB)

---
+++ trail1/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        w += 8;
+        h *= 3;
+        System.out.println(w);
+        System.out.println(h);
+        System.out.println(w * h);
+    }
+}
