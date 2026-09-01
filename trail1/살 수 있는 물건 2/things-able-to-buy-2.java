e066d9b15be7a74002367f1053c13b5c8ac17ac7
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-01 13:37:20 +0900

    [Passed] 살 수 있는 물건 2 (142ms, 10MB)

---
+++ trail1/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int cash = sc.nextInt();
+
+        if(cash >= 3000){
+            System.out.println("book");
+        }
+        else if(cash >= 1000){
+            System.out.println("mask");
+        }
+        else if(cash >= 500){
+            System.out.println("pen");
+        }
+        else{
+            System.out.println("no");
+        }
+    }
+}
