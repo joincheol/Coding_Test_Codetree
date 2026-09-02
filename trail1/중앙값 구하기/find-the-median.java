39429623e5f0acfe77647595a77674d649ab5248
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-02 23:33:44 +0900

    [Passed] 중앙값 구하기 (139ms, 10MB)

--- trail1/중앙값 구하기/find-the-median.java
+++ trail1/중앙값 구하기/find-the-median.java
@@ -8,28 +8,26 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if(a > b && a > c){
-            if(b > c){
+        if(a > b){
+            if(c > a){
+                System.out.println(a);
+            }
+            else if(b > c){
                 System.out.println(b);
             }
             else{
                 System.out.println(c);
             }
         }
-        else if(b > a && b > c){
-            if(a > c){
+        else{
+            if(c > b){
+                System.out.println(b);
+            }
+            else if(a > c){
                 System.out.println(a);
             }
             else{
                 System.out.println(c);
-            }
-        }
-        else if(c > a && c > b){
-            if(a > b){
-                System.out.println(a);
-            }
-            else{
-                System.out.println(b);
             }
         }
     }
