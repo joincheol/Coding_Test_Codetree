e56215ee3638c68391d16cd2129f78d33b69ca63
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-03 14:01:32 +0900

    [Passed] 배수의 개수 (129ms, 10MB)

---
+++ trail1/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for(int i=0; i<10; i++){
+            int num = sc.nextInt();
+            if(num % 3 == 0){
+                cnt1++;
+            }
+            if(num % 5 == 0){
+                cnt2++;
+            }
+        }
+        System.out.println(cnt1+" "+cnt2);
+    }
+}
