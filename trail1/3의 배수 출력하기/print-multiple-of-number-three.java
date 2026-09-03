a4d00f008d22092f5c96723b61cbce213727966a
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-03 10:00:23 +0900

    [Passed] 3의 배수 출력하기 (129ms, 10MB)

---
+++ trail1/3의 배수 출력하기/print-multiple-of-number-three.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+
+        while(i <= n){
+            if(i % 3 == 0){
+                System.out.print(i+" ");
+            }
+            i++;
+        }
+    }
+}
