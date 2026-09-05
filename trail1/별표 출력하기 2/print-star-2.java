21428c3be069c361062692c8bf760b728f3d03a3
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-05 19:26:45 +0900

    [Passed] 별표 출력하기 2 (144ms, 10MB)

---
+++ trail1/별표 출력하기 2/print-star-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=n; j>i; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
