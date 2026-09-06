3493c812b817e1e017c47a1d59090d50405023e4
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-06 18:47:46 +0900

    [Passed] 특정 규칙에 따른 문자 출력 (132ms, 10MB)

---
+++ trail1/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 1;
+
+        for(int i=0; i<2*n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                System.out.print("  ");
+            }
+            for(int j=0; j<cnt; j++){
+                System.out.print("@ ");
+            }
+            System.out.println();
+            if(i >= n-1){
+                cnt--;
+            }
+            else{
+                cnt++;
+            }
+        }
+    }
+}
