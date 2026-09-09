72053009ff4f23eb2f2ebe48d921bee305209b27
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-09 11:07:25 +0900

    [Passed] 좌우 지그재그 출력 (133ms, 10MB)

--- trail1/좌우 지그재그 출력/print-lr-zigzag.java
+++ trail1/좌우 지그재그 출력/print-lr-zigzag.java
@@ -7,19 +7,14 @@ public class Main {
         int n = sc.nextInt();
 
         for(int i=0; i<n; i++){
-            int cnt = 1;
             if(i % 2 == 0){
-                cnt = cnt+(i*n);
-                for(int j=0; j<n; j++){
-                    System.out.print(cnt+" ");
-                    cnt++;
+                for(int j=1; j<=n; j++){
+                    System.out.print(n*i+j+" ");
                 }
             }
             else{
-                cnt = (i+1) * n;
                 for(int j=0; j<n; j++){
-                    System.out.print(cnt+" ");
-                    cnt--;
+                    System.out.print(n*(i+1) - j+" ");
                 }
             }
             System.out.println();
