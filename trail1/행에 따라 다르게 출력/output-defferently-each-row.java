7f3547caf78dc8ce373ae9249e9cbed899a143d6
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-09 14:43:52 +0900

    [Passed] 행에 따라 다르게 출력 (133ms, 10MB)

---
+++ trail1/행에 따라 다르게 출력/output-defferently-each-row.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i % 2 == 0){
+                    cnt++;
+                    System.out.print(cnt+" ");
+                }
+                if(i % 2 == 1){
+                    cnt+=2;
+                    System.out.print(cnt+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
