e60929913d07b843c152ea82883ab5bfab7e67c4
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-09 15:30:43 +0900

    [Passed] 19단 출력 (120ms, 10MB)

---
+++ trail1/19단 출력/nineteen-times-table.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int n = 19;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(j % 2 == 0){
+                    System.out.println(i+" * "+j+" = "+i*j);
+                }
+                else{
+                    System.out.print(i+" * "+j+" = "+i*j);
+                    if(j <= 17){
+                        System.out.print(" / ");
+                    }
+                }
+                if(j == 19){
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
