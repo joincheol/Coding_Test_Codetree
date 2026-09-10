ba095dbad4fc1f26249f85940af20647328a30d6
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-10 14:21:07 +0900

    [Passed] 완전수 (151ms, 10MB)

---
+++ trail1/완전수/perfect-number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int count = 0;
+
+        for(int i=start; i<=end; i++){
+            int total = 0;
+
+            for(int j=1; j<i; j++){
+                if(i % j == 0){
+                    total += j;
+                }
+            }
+            if(total == i){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
