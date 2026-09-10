cc536e222425cc0922c09ad5cbf51913cef98972
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-10 13:25:20 +0900

    [Passed] 알파벳 출력하기 (132ms, 10MB)

---
+++ trail1/알파벳 출력하기/print-alphabet.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char c = 'A';
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(c);
+                c++;
+                if(c >= 91){
+                    c = 'A';
+                }
+            }
+            System.out.println();
+        }
+    }
+}
