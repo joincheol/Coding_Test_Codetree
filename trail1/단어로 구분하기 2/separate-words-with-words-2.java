462cefa7094e90a181925d16f336649cbb833a36
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-12 20:56:36 +0900

    [Passed] 단어로 구분하기 2 (124ms, 10MB)

---
+++ trail1/단어로 구분하기 2/separate-words-with-words-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String[] arr = new String[11];
+
+        for(int i=1; i<=10; i++){
+            arr[i] = sc.next();
+            if(i % 2 == 1){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
