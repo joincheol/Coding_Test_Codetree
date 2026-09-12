a65b17b2cd76c3bb36f9e024f551b4d1aabc4ba5
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-12 13:46:27 +0900

    [Passed] 가장 짧은 문자열 (133ms, 10MB)

--- trail1/가장 짧은 문자열/shortest-string.java
+++ trail1/가장 짧은 문자열/shortest-string.java
@@ -11,39 +11,15 @@ public class Main {
         int len1 = str1.length();
         int len2 = str2.length();
         int len3 = str3.length();
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
 
-        if(len1 > len2){
-            if(len1 > len3){
-                max = len1;
-                if(len2 > len3){
-                    min = len3;
-                }
-                else{
-                    min = len2;
-                }
-            }
-            else{
-                max = len3;
-                min = len2;
-            }
-        }
-        else{
-            if(len2 > len3){
-                max = len2;
-                if(len3 > len1){
-                    min = len1;
-                }
-                else{
-                    min = len3;
-                }
-            }
-            else{
-                max = len3;
-                min = len1;
-            }
-        }
+        int max = len1;
+        if(max < len2) max=len2;
+        if(max < len3) max=len3;
+
+        int min = len1;
+        if(min > len2) min=len2;
+        if(min > len3) min=len3;
+
         System.out.println(max - min);
     }
 }
