5c65fe9410d7f231ca31dcb322d84c3e4ca3a9ec
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-14 17:54:05 +0900

    [Passed] 부분 문자열의 개수 (132ms, 10MB)

--- trail1/부분 문자열의 개수/number-of-substrings.java
+++ trail1/부분 문자열의 개수/number-of-substrings.java
@@ -8,7 +8,7 @@ public class Main {
         String b = sc.next();
         int cnt = 0;
         
-        for(int i=0; i<a.length(); i++){
+        for(int i=0; i<a.length()-1; i++){
             if(a.charAt(i) == b.charAt(0) && a.charAt(i+1) == b.charAt(1)){
                 cnt++;
             }
