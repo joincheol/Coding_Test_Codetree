9f31bc9d9d5005c55ec1bebd77de891e10106de8
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-14 17:28:01 +0900

    [Passed] 특정 문자의 유무 (139ms, 10MB)

---
+++ trail1/특정 문자의 유무/specific-character-presence.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String status1 = "No";
+        String status2 = "No";
+
+        for(int i=0; i<str.length()-3; i++){
+            if(str.substring(i,i+2).equals("ee")){
+                status1 = "Yes";
+            }
+            if(str.substring(i,i+2).equals("ab")){
+                status2 = "Yes";
+            }
+        }
+        System.out.println(status1+" "+status2);
+    }
+}
