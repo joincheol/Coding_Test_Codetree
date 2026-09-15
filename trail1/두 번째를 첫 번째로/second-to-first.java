bf723d3aad28fdf2f61bf1ddc3f2cdc0036734df
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-15 15:02:23 +0900

    [Passed] 두 번째를 첫 번째로 (130ms, 10MB)

---
+++ trail1/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] c = str.toCharArray();
+        char first = c[0];
+        char second = c[1];
+
+        for(int i=0; i<str.length(); i++){
+            if(c[i] == second){
+                c[i] = first;
+            }
+        }
+        str = String.valueOf(c);
+        System.out.println(str);
+    }
+}
