a31b13321868d4ed9387b4510325c26dc5f0e3fe
joincheol <66054870+joincheol@users.noreply.github.com>
2026-09-18 17:10:33 +0900

    [Passed] 정수만 추출하기 (126ms, 10MB)

---
+++ trail1/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        String temp1 = "";
+        String temp2 = "";
+
+        for(int i=0; i<str1.length(); i++){
+            if(str1.charAt(i) >= '0' & str1.charAt(i) <= '9'){
+                temp1 += str1.charAt(i);
+            }
+            else{
+                break;
+            }
+        }
+        
+        for(int i=0; i<str2.length(); i++){
+            if(str2.charAt(i) >= '0' & str2.charAt(i) <= '9'){
+                temp2 += str2.charAt(i);
+            }
+            else{
+                break;
+            }
+        }
+        System.out.println(Integer.parseInt(temp1) + Integer.parseInt(temp2));
+    }
+}
